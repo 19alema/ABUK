34521985c7042f04f10106d3d4a9b85bd2401bda
19alema <75095003+19alema@users.noreply.github.com>
2021-08-23 21:47:04 +0300

    update

---
+++ app.js
@@ -0,0 +1,13 @@
+
+window.addEventListener("mouseover",play);
+window.addEventListener("click",play);
+function play() { document.getElementById("audio").play();
+}
+
+
+const butn = document.getElementById("button");
+
+function E() {
+  alert("HappyBirthday Abuk hope you will find time to make the day so memorable and joyful...... HAPPY BIRTHDAY 🥳 <-CHAPLAIN->       ||    HAPPYBIRTHDAY My friend, it has been along while since when i last spoke to you.. wishing you all the best in this life journey🥳🥳🌹🌹...<-ANDREW->     || Seeing you happy makes me happy, hoping to see you aging responsibly, happyBirthday🥳🌹 my friend  ..<-OWINYI'S FRIEND EDRICK->")
+}
+
